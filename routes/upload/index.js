f372ae291c0a18c9120c74836f7c71701a510c50
dosbenjamin <dosbenjamin@gmail.com>
2021-03-27 22:23:46 +0100

    feat(cloudinary): add upload to cloud storage

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -1,8 +1,19 @@
 import multer from 'fastify-multer'
+import cloudinary from '../../utilities/cloudinary.js'
+
+const cloudUpload = ({ buffer }) => {
+  const file = `data:image/png;base64,${ buffer.toString('base64') }`
+
+  return cloudinary.uploader
+    .unsigned_upload(file, 'jyeiofw9', {
+      resource_type: 'raw',
+      folder: 'jungle-soundbot/'
+    })
+}
 
 export default async fastify => {
   const storage = multer.memoryStorage()
-  const upload = multer({ storage: storage })
+  const upload = multer({ storage })
 
   fastify.route({
     method: 'POST',
@@ -10,9 +21,10 @@ export default async fastify => {
     preHandler: upload.single('sound'),
     handler: async ({ body: { author, command }, file }) => {
       const { knex } = await fastify.bookshelf
+      const { url } = await cloudUpload(file)
 
       return knex('sounds')
-        .insert({ command, author })
+        .insert({ command, author, url })
     }
   })
 }
