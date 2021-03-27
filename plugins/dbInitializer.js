e8a675dd55e6892b54a974660009ac2ac18d7139
dosbenjamin <dosbenjamin@gmail.com>
2021-03-27 22:22:40 +0100

    refactor(database): better destructuring + change command name key

--- plugins/dbInitializer.js
+++ plugins/dbInitializer.js
@@ -1,11 +1,11 @@
 export default async ({ bookshelf }) => {
-  const { knex } = await bookshelf
-  const soundsExists = await knex.schema.hasTable('sounds')
+  const { schema } = await bookshelf.knex
+  const soundsExists = await schema.hasTable('sounds')
 
   return !soundsExists
-    && knex.schema.createTable('sounds', table => {
+    && schema.createTable('sounds', table => {
       table.increments('id').primary()
-      table.string('name')
+      table.string('command').unique()
       table.string('url')
       table.string('author')
     })
